2600583755dec49bbd78d04c707ce858f529c6d7
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-03-11 20:52:06 -0300

    alteração na estrututura da tabela

--- creatingTables.sql
+++ creatingTables.sql
@@ -18,18 +18,17 @@ create table dadosSensor(
 
 create table montadora(
 	idMontadora int primary key auto_increment,
-    nomeMontadora varchar(70),
+    nomeMontadora varchar(70) not null,
     cnpjMontadora char(18) unique not null,
     emailMontadora varchar(50) unique not null,
-    token varchar(50) unique not null
+    token varchar(255) unique not null
 );
 
 create table funcionarioMontadora(
 	idFuncionarioMontadora int primary key auto_increment,
-    nomeFuncionario varchar(50),
+    nomeFuncionario varchar(50) not null,
     emailFuncionario varchar(60) unique,
     senhaFuncionario varchar(50),
-    cargoFuncionario varchar(50),
     imagemFuncionario varchar(100) default(null),
     idMontadora int,
     constraint fk_idMontadora foreign key(idMontadora) references montadora(idMontadora)
@@ -37,7 +36,7 @@ create table funcionarioMontadora(
 
 create table carro(
 	idCarro int primary key auto_increment,
-    modeloCarro varchar(100),
+    modeloCarro varchar(100) not null,
     anoCarro varchar(9),
     placaCarro varchar(15) unique,
     idSensor int,
